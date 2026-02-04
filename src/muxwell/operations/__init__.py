869542fb0b6f903eb603adffbb49bb5ea4db4f31
Matthieu Daubie <matthieu.daubie@outlook.com>
2026-02-04 12:00:42 +0100

    feat(apply): implement add subtitle operation

--- src/muxwell/operations/__init__.py
+++ src/muxwell/operations/__init__.py
@@ -1,4 +1,5 @@
+from .add_subs import AddSubtitles
 from .base import Operation
 from .global_ops import SetTitle
 
-__all__ = ["Operation", "SetTitle"]
+__all__ = ["AddSubtitles", "Operation", "SetTitle"]
