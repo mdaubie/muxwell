10f483582d05da3a5bf8e48e621b1b0678bd0191
Matthieu Daubie <matthieu.daubie@outlook.com>
2026-02-05 12:22:03 +0100

    fix(apply): validate extension when adding subs

--- src/muxwell/actions/add_subs.py
+++ src/muxwell/actions/add_subs.py
@@ -2,7 +2,10 @@
 
 from pathlib import Path
 
+import typer
+
 from ..operations import AddSubtitles
+from ..utils.files import SUBTITLES_EXTENSIONS
 from .base import Action, ActionContext
 
 
@@ -10,6 +13,10 @@ class AddSubtitlesAction(Action):
     """Add subtitles track"""
 
     def __init__(self, path: Path):
+        if path.suffix.lower() not in SUBTITLES_EXTENSIONS:
+            raise typer.BadParameter(
+                f"Unsupported subtitles format: {path.suffix}, supported formats are: {', '.join(SUBTITLES_EXTENSIONS)}"
+            )
         self.path = path
 
     def analyze(self, context: ActionContext):
